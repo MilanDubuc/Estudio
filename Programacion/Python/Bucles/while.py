a1dac70ace494e11650e837af6e6617dab79e347
MilanDubuc <milanndbc@gmail.com>
2024-01-05 00:50:59 -0300

    siu

---
+++ Programacion/Python/Bucles/while.py
@@ -0,0 +1,11 @@
+#Whie SIEMPRE se ejecuta mientras la condicion sea verdadera
+
+a=0
+print("huh %s" %(a))
+#entra al while:
+while a < 10:
+    a += 1
+    print(a)
+#una vez que salio no vuelve a arriba; el codigo sigue hacia abajo y termina
+a -= 1
+print("hmm %s" %(a))
---
+++ Programacion/Python/funciones/funciones.py
@@ -0,0 +1,13 @@
+#built ins de python:
+
+# max(lista), devuelve el numero mas alto de la lista
+# min(lista), "" el mas chico
+
+# round(float, cant de decimales) ; redondea a la cant de decimales especificada
+
+# bool(dato) ; cambia el tipo de dato (bool(12) devuelve true ; bool(0) devuelve false ; etc)
+# int() ; ""
+# etc.
+
+
+
