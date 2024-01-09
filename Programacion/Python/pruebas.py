b3f7058afee548fa551ec16d0502f2f8e63a7986
MilanDubuc <milanndbc@gmail.com>
2024-01-09 17:27:50 -0300

    9/1/24 v2

--- Programacion/Python/pruebas.py
+++ Programacion/Python/pruebas.py
@@ -1,4 +1,4 @@
-
+""" Ej.1 video del bobo
 lista = [4, 6, 7, 4, 3, 2, 3, 4]
 cuco = 4
 
@@ -14,3 +14,20 @@ print(pepe(lista, cuco))
 print(lista)
 
 print(dir(lista))
+"""
+
+"""ordena segun cant de vocales
+def clave(x):
+    vocales = ["a", "e", "i", "o", "u"]
+    cont = 0
+    for i in range(5):
+        cont += x[0].count(vocales[i-1])
+    return cont
+
+
+lista = [("hola puto", 12), ("alohe", 22), ('zorro', 5)]
+
+lista.sort(key=clave)
+print(lista)
+"""
+
