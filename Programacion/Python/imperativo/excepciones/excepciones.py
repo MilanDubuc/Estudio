78848859437f36a1b51495048467b485b07449d6
MilanDubuc <milanndbc@gmail.com>
2024-01-11 00:56:20 -0300

    Thu, Jan 11, 2024 12:56:17 AM':

--- Programacion/Python/imperativo/excepciones/excepciones.py
+++ Programacion/Python/imperativo/excepciones/excepciones.py
@@ -1,5 +1,5 @@
 #con try intenta ejecutar el codigo, si tira una excepcion, aborta y ejecuta lo de except
-def sumar()
+def sumar():
     while True:
         a = int(input("Ingrese numero 1: "))
         b = int(input("Ingrese numero 2: "))
